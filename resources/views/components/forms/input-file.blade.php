08d3ae0dbe7bdf5ea57e09e2bff5b06cf28f8c62
121140111_Bendry Lakburlawal <bendrylakburlawal01@gmail.com>
2024-07-21 16:50:57 +0700

    update: make component

---
+++ resources/views/components/forms/input-file.blade.php
@@ -0,0 +1,56 @@
+@props(['placeholder' => 'Upload File', 'name', 'accept' => 'application/pdf'])
+@php
+    $id = $name;
+@endphp
+<div>
+    <div id="{{$id}}-upload" data-id="{{$id}}"
+        class="max-w-file-input border-1 flex bg-main-white border-input-focus p-8px justify-center items-center font-semibold rounded-5 text-input-focus cursor-pointer">
+        <x-phosphor-upload-simple-bold class="w-5 h-5" id="{{$id}}-icon" />
+        <input id="{{$id}}-fileName" class="text-center w-[50%] cursor-pointer bg-transparent" type="text"
+            value="{{$placeholder}}" readonly>
+    </div>
+    <input type="file" id="{{$id}}" name="{{$name}}" class="hidden" {{$attributes}} accept="{{$accept}}" />
+    <div id="{{$id}}-error" class="text-red-500 mt-2 hidden"></div> <!-- Error message element -->
+</div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const uploadElement = document.getElementById('{{$id}}-upload');
+        const inputElement = document.getElementById('{{$id}}');
+        const fileNameElement = document.getElementById('{{$id}}-fileName');
+        const uploadIcon = document.getElementById('{{$id}}-icon');
+        const errorElement = document.getElementById('{{$id}}-error');
+        const maxFileSize = 1 * 1024 * 1024;
+
+        uploadElement.addEventListener('click', function () {
+            inputElement.click();
+        });
+
+        inputElement.addEventListener('change', function () {
+            if (inputElement.files.length > 0) {
+                const file = inputElement.files[0];
+                if (file.size > maxFileSize) {
+                    errorElement.textContent = 'File size exceeds 1 MB';
+                    errorElement.classList.remove('hidden');
+                    fileNameElement.value = '{{$placeholder}}';
+                    uploadIcon.classList.remove('hidden');
+                    fileNameElement.classList.remove('w-full');
+                    fileNameElement.classList.add('w-[50%]');
+                } else {
+                    const fileName = file.name;
+                    fileNameElement.value = fileName;
+                    fileNameElement.classList.remove('w-[50%]');
+                    fileNameElement.classList.add('w-full');
+                    uploadIcon.classList.add('hidden');
+                    errorElement.classList.add('hidden');
+                }
+            } else {
+                fileNameElement.value = '{{$placeholder}}';
+                uploadIcon.classList.remove('hidden');
+                fileNameElement.classList.remove('w-full');
+                fileNameElement.classList.add('w-[50%]');
+                errorElement.classList.add('hidden');
+            }
+        });
+    })
+</script>
---
+++ resources/views/components/forms/input.blade.php
@@ -0,0 +1,14 @@
+@props(['name', 'id', 'textarea' => false])
+@php
+    $id = $name;
+@endphp
+<div>
+    @if($textarea)
+        <textarea name="{{$name}}" id="{{$id}}" {{$attributes->class(['border-1 border-input-border shadow-input-shadow rounded-10 py-10px px-12px w-full focus:border-input-focus placeholder:text-input-placeholder placeholder:font-semibold'])}} data-role="tagsinput"></textarea>
+    @else
+        <input name="{{$name}}" id="{{$id}}" {{$attributes->class(['border-1 border-input-border shadow-input-shadow rounded-10 py-10px px-12px w-full h-normal-input focus:border-input-focus placeholder:text-input-placeholder placeholder:font-semibold'])->merge(['type' => 'text'])}} data-role="tagsinput" />
+    @endif
+    @error($name)
+        <small class="invalid-feedback font-5" style="margin-top: -10px; display: block;">{{ $message }}</small>
+    @enderror
+</div>
